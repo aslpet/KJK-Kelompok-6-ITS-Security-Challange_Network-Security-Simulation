f1504ed08066cbfab90f2caa12764eb407fee1fb
aslpet <ahmadrafituturu@gmail.com>
2025-11-22 11:32:23 +0700

    osis

--- Config/Router/AcademicR.sh
+++ Config/Router/AcademicR.sh
@@ -12,4 +12,12 @@
 
 # Default Route
 /ip route add dst-address=0.0.0.0/0 gateway=192.168.20.1
+
+/ip firewall filter add chain=forward src-address=10.20.30.0/24 dst-address=10.20.20.0/24 action=drop comment="BLOCK IoT to Academic Server (Local Isolation)"
+
+# 1. Izinkan DNS dari Zona Akademik (Wajib supaya bisa resolve domain)
+/ip firewall filter add chain=forward action=accept protocol=udp in-interface=ether5 out-interface=ether1 dst-port=53 place-before=20 comment="Academic DNS"
+
+# 2. Izinkan HTTP & HTTPS dari Zona Akademik ke Internet (Untuk apt-get update)
+/ip firewall filter add chain=forward action=accept protocol=tcp in-interface=ether5 out-interface=ether1 dst-port=80,443 place-before=20 comment="Academic Server Update"
 # --- SCRIPT END AcademicR ---
